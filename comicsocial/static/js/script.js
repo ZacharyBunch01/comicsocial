418b5be982efe5985520ca606dad9fdb66a291b6
Mug-Costanza <77304096+Mug-Costanza@users.noreply.github.com>
2022-08-06 11:06:07 -0400

    Add files via upload

--- comicsocial/static/js/script.js
+++ comicsocial/static/js/script.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("paint");
 var image = document.getElementById("image");
+var dropRegion = document.getElementById("drop-region");
+var imagePreviewRegion = document.getElementById("image-preview");
 var ctx = canvas.getContext("2d");
-var width = canvas.width, height = canvas.height;
 var curX, curY, prevX, prevY;
 var mousePos = { x : 0, y : 0 };
 var lastPos = mousePos;
@@ -12,6 +13,84 @@ var canvasData = {"pencil": [], "line": [], "rectangle": [], "circle": [], "eras
 ctx.lineWidth = 2;
 var font = '14px sans-serif';
 var hasInput = false;
+
+function highlight()
+{
+    dropRegion.classList.add('highlighted');
+}
+
+function unhighlight()
+{
+    dropRegion.classList.remove("highlighted");
+}
+
+// Change canvas size according to template
+
+var templateImage = new Image();
+
+if(document.URL.includes("1"))
+{
+    canvas.width = 600;
+    canvas.height = 600;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 600;
+    templateImage.src = '../../../media/uploads/assets/template1.png';
+}
+else if(document.URL.includes("2"))
+{
+    canvas.width = 600;
+    canvas.height = 600;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 600;
+    templateImage.src = '../../../media/uploads/assets/template8.png';
+}
+else if(document.URL.includes("3"))
+{
+    canvas.width = 600;
+    canvas.height = 600;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 600;
+    templateImage.src = '../../../media/uploads/assets/template9.png';
+}
+else if(document.URL.includes("4"))
+{
+    canvas.width = 600;
+    canvas.height = 900;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 900;
+    templateImage.src = '../../../media/uploads/assets/template2.png';
+}
+else if(document.URL.includes("5"))
+{
+    canvas.width = 600;
+    canvas.height = 900;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 900;
+    templateImage.src = '../../../media/uploads/assets/template6.png';
+}
+else if(document.URL.includes("6"))
+{
+    canvas.width = 900;
+    canvas.height = 600;
+    ctx.canvas.width = 900;
+    ctx.canvas.height = 600;
+    templateImage.src = '../../../media/uploads/assets/template3.png';
+}
+else if(document.URL.includes("7"))
+{
+    canvas.width = 900;
+    canvas.height = 600;
+    ctx.canvas.width = 900;
+    ctx.canvas.height = 600;
+    templateImage.src = '../../../media/uploads/assets/template7.png';
+}
+
+templateImage.onload = function()
+{
+    ctx.drawImage(templateImage, 0, 0);
+}
+
+var width = canvas.width, height = canvas.height;
 
 // Add a white background, otherwise background displays as transparent
 var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -29,6 +108,238 @@ for(var i = 0; i < data.length; i += 4)
 }
 
 ctx.putImageData(imgData, 0, 0);
+
+// Image Uploader
+var fakeInput = document.createElement("input");
+fakeInput.type = "file";
+fakeInput.accept = "image/*";
+fakeInput.multiple = true;
+dropRegion.addEventListener('click', function()
+{
+    fakeInput.click();
+});
+
+fakeInput.addEventListener("change", function()
+{
+    var files = fakeInput.files;
+    handleFiles(files);
+});
+
+function preventDefault(e)
+{
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+dropRegion.addEventListener('dragenter', preventDefault, false);
+dropRegion.addEventListener('dragleave', preventDefault, false);
+dropRegion.addEventListener('dragover', preventDefault, false);
+dropRegion.addEventListener('drop', preventDefault, false);
+
+canvas.addEventListener('dragenter', preventDefault, false);
+canvas.addEventListener('dragleave', preventDefault, false);
+canvas.addEventListener('dragover', preventDefault, false);
+canvas.addEventListener('drop', preventDefault, false);
+
+function handleFiles(files)
+{
+    for(var i = 0, len = files.length; i < len; i++)
+    {
+        if(validateImage(files[i]))
+            previewAnduploadImage(files[i]);
+    }
+}
+
+function handleDrop(e)
+{
+    if(e.target == dropRegion && e.target != canvas)
+    {
+        var dt = e.dataTransfer;
+        var files = dt.files;
+            
+        if(files.length)
+        {
+            handleFiles(files);
+        }
+        else
+        {
+            var html = dt.getData('text/html');
+            var match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html);
+            var url = match && match[1];
+                
+            if(url)
+            {
+                uploadImageFromURL(url);
+                return;
+            }
+        }
+            
+        function uploadImageFromURL(url)
+        {
+            var img = new Image;
+                
+            img.onload = function()
+            {
+                var c = document.createElement("dropRegionCanvas");
+                var ctxDropRegion = document.getContext("2d");
+                ctxDropRegion.drawImage(this, 0, 0);
+                c.toBlob(function(blob)
+                {
+                    handleFiles( [blob] );
+                }, "image/png");
+            };
+                
+            img.onerror = function()
+            {
+                alert("There was an error uploading the image.");
+            }
+                
+            img.crossOrigin = "";
+            img.src = url;
+        }
+    }
+}
+
+dropRegion.addEventListener('drop', handleDrop, false);
+
+var guy1URL = "../../../media/uploads/assets/Guy11.png";
+
+function canvasHandleDrop(e)
+{
+    if(e.target == canvas && e.target != dropRegion)
+    {
+        var dt = e.dataTransfer;
+        var files = dt.files;
+            
+        function uploadImageFromURL(url)
+        {
+            var img = new Image;
+                
+            img.onload = function()
+            {
+                ctx.drawImage(this, e.clientX - canvas.getBoundingClientRect().left - (this.width * 0.25 * 0.5), e.clientY - canvas.getBoundingClientRect().top - (this.height * 0.25 * 0.5), this.width * 0.25, this.height * 0.25);
+            };
+                
+            img.onerror = function()
+            {
+                alert("There was an error uploading the image.");
+            }
+                
+            img.crossOrigin = "";
+            img.src = url;
+        }
+        
+        if(files.length)
+        {
+            handleFiles(files);
+        }
+        else
+        {
+            var html = dt.getData('text/html');
+            var match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html);
+            var url = match && match[1];
+                
+            if(url)
+            {
+                uploadImageFromURL(url);
+                return;
+            }
+        }
+    }
+}
+
+canvas.addEventListener('drop', canvasHandleDrop, false);
+
+function validateImage(image)
+{
+    var validTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    
+    if(validTypes.indexOf( image.type ) === -1)
+    {
+        alert("Invalid File Type");
+        return false;
+    }
+    
+    var maxSizeInBytes = 10e6;
+    
+    if(image.size > maxSizeInBytes)
+    {
+        alert("Image file is too large.");
+        return false;
+    }
+    
+    return true;
+}
+
+function previewAnduploadImage(image)
+{
+    var imgView = document.createElement("div");
+    imgView.className = "image-view";
+    imagePreviewRegion.appendChild(imgView);
+    
+    var img = document.createElement("img");
+    imgView.appendChild(img);
+    
+    var overlay = document.createElement("div");
+    overlay.className = "overlay";
+    imgView.appendChild(overlay);
+
+    var reader = new FileReader();
+
+    reader.onload = function(e)
+    {
+        img.src = e.target.result;
+    }
+
+    reader.readAsDataURL(image);
+
+    var formData = new FormData();
+    formData.append('image', image);
+
+    var uploadLocation = ''; // URL of upload handler.
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", uploadLocation, true);
+
+    ajax.onreadystatechange = function(e)
+    {
+        if(ajax.readyState === 4)
+        {
+            if(ajax.status === 200)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    ajax.upload.onprogress = function(e)
+    {
+        var perc = (e.loaded / e.total * 100) || 100, width = 100 - perc;
+        
+        overlay.style.width = width;
+    }
+
+    ajax.send(formData);
+}
+
+function detectDragDrop()
+{
+    var div = document.createElement('div');
+    return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
+}
+
+var dragSupported = detectDragDrop();
+
+if(!dragSupported)
+{
+    document.getElementByClassName("drop-message")[0].innerHTML = 'Click to upload';
+}
+
+// Drag images from image set onto canvas.
 
 canvas.addEventListener("touchstart", function (e)
 {
@@ -157,6 +468,8 @@ function reset()
     }
 
     ctx.putImageData(imgData, 0, 0);
+    
+    ctx.drawImage(templateImage, 0, 0);
 }
 
 function pencil()
